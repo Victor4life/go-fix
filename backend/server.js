f2e86f2dac3ea1a67e6d55128233959a8317b958
Emeka Victor <emekav233@gmail.com>
2025-04-14 20:37:00 +0100

    commitment

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const limiter = rateLimit({
 
 // Enhanced CORS configuration
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://victor4life.github.io/go-fix/ "],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  base: "/go-fix/", // Add this line - replace 'go-fix' with your repository name
   plugins: [react()],
   assetsInclude: ["**/*.lottie"],
   build: {
