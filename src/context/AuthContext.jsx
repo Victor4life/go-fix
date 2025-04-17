b2a68c0f02756914a3d9e2f8219ba82762ca6036
Emeka Victor <emekav233@gmail.com>
2025-04-17 08:52:46 +0100

    auth and env configuration

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,8 @@ import { createContext, useState, useContext, useEffect } from "react";
 
 const AuthContext = createContext();
 
-const API_BASE_URL = "http://localhost:5000/api/auth";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/auth";
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
